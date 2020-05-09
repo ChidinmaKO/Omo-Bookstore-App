95f2387a254d6c596b776d259f0292fb84114c61
ChidinmaKO <kchidinmavictoria@gmail.com>
2020-05-09 13:04:46 +0200

    add vue setup

---
+++ client/src/components/Ping.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      msg: 'Hello World Class Engineer!',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://127.0.0.1:4000/ping';
+      axios.get(path).then((response) => {
+        this.msg = response.data;
+      }).catch((error) => {
+        // eslint-disable-next-line
+        console.error(error);
+      });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
