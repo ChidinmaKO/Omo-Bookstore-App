05f62800507294416560e4c4a2fefe1c8712553e
ChidinmaKO <kchidinmavictoria@gmail.com>
2020-05-09 12:34:55 +0200

    add flask setup

---
+++ omobookstore/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+from omobookstore.config import Config
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(__name__)
+
+    # enable CORS
+    CORS(app, resources={r'/*': {'origins': '*'}})
+
+    @app.route('/ping', methods=['GET'])
+    def ping_pong():
+        return jsonify('pong!')
+
+    return app
---
+++ omobookstore/config.py
@@ -0,0 +1,4 @@
+import os
+
+class Config:
+    SECRET_KEY = "a26385d494a3303155d3a2bd88f9af0a5decfd38a6201c130bc62af974f5a9ff"
