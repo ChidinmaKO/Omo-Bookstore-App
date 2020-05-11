98dc38e1001b8440be61e541e77e57e9fac457fd
ChidinmaKO <kchidinmavictoria@gmail.com>
2020-05-11 09:02:30 +0200

    add put route and update book method

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -29,7 +29,14 @@
               </td>
               <td>
                 <div class="btn-group" role="group">
-                  <button type="button" class="btn btn-warning btn-sm">Update</button>
+                  <button
+                    type="button"
+                    class="btn btn-warning btn-sm"
+                    v-b-modal.book-update-modal
+                    @click="editBook(book)"
+                  >
+                    Update
+                  </button>
                   <button type="button" class="btn btn-danger btn-sm">Delete</button>
                 </div>
               </td>
@@ -99,15 +106,78 @@
         <b-button type="reset" variant="danger">Reset</b-button>
       </b-form>
     </b-modal>
+
+    <!-- TODO: Move to UpdateModal Component -->
+    <update-book-modal
+      id="book-update-modal"
+    >
+      <b-modal ref="editBookModal"
+        id="book-update-modal"
+        title="Update"
+        hide-footer
+      >
+        <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+          <b-form-group
+            id="form-title-edit-group"
+            label="Title:"
+            label-for="form-title-edit-input"
+          >
+            <b-form-input
+              id="form-title-edit-input"
+              type="text"
+              v-model="editForm.title"
+              required
+              placeholder="Enter title"
+            >
+            </b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="form-author-edit-group"
+            label="Author:"
+            label-for="form-author-edit-input"
+          >
+            <b-form-input
+              id="form-author-edit-input"
+              type="text"
+              v-model="editForm.author"
+              required
+              placeholder="Enter author"
+            >
+            </b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="form-read-edit-group"
+          >
+            <b-form-checkbox-group
+              v-model="editForm.read"
+              id="form-checks"
+            >
+              <b-form-checkbox value="true">Read?</b-form-checkbox>
+            </b-form-checkbox-group>
+          </b-form-group>
+
+          <b-button-group>
+            <b-button type="submit" variant="primary">Update</b-button>
+            <b-button type="reset" variant="danger">Cancel</b-button>
+          </b-button-group>
+
+        </b-form>
+      </b-modal>
+    </update-book-modal>
+
   </b-container>
 </template>
 
 <script>
 import globalAxios from 'axios';
 import Alert from './Alert.vue';
+import UpdateBookModal from './UpdateBookModal.vue';
 
 export default {
   data() {
+    // TODO: change `read` to a boolean not array
     return {
       addBookForm: {
         title: '',
@@ -117,11 +187,17 @@ export default {
       books: [],
       showAlert: false,
       message: '',
+      editForm: {
+        title: '',
+        author: '',
+        read: [],
+      },
     };
   },
 
   components: {
     'app-alert': Alert,
+    'update-book-modal': UpdateBookModal,
   },
 
   methods: {
@@ -137,9 +213,14 @@ export default {
     },
 
     initForm() {
-      this.addBookForm.title = '';
-      this.addBookForm.author = '';
-      this.addBookForm.read = false;
+      if (this.$refs.addBookModal) {
+        this.addBookForm.title = '';
+        this.addBookForm.author = '';
+        this.addBookForm.read = [];
+      }
+      this.editForm.title = '';
+      this.editForm.author = '';
+      this.editForm.read = [];
     },
 
     onSubmit(e) {
@@ -165,6 +246,8 @@ export default {
     },
 
     addBook(payload) {
+      // Todo: check if book already exists
+      // Todo: get book title
       globalAxios.post('/books', payload).then((response) => {
         // eslint-disable-next-line
         console.log('response: ', response);
@@ -180,8 +263,45 @@ export default {
       });
     },
 
-    updateBook() {
-      this.showAlert = true;
+    editBook(book) {
+      this.editForm = book;
+    },
+
+    onSubmitUpdate(e) {
+      e.preventDefault();
+      this.$refs.editBookModal.hide();
+      let read = false;
+      if (this.editForm.read[0]) read = true;
+      const payload = {
+        title: this.editForm.title,
+        author: this.editForm.author,
+        read,
+      };
+      this.updateBook(payload, this.editForm.id);
+    },
+
+    updateBook(payload, bookId) {
+      // Todo: get book title
+      globalAxios.put(`/books/${bookId}`, payload).then((response) => {
+        // eslint-disable-next-line
+        console.log('response: ', response);
+        this.getBooks();
+        this.showAlert = true;
+        this.message = 'Book updated!';
+      }).catch((error) => {
+        // eslint-disable-next-line
+        console.error(error);
+        this.getBooks();
+        this.showAlert = true;
+        this.message = 'An error occurred whilst updating this book. Please try again.';
+      });
+    },
+
+    onResetUpdate(e) {
+      e.preventDefault();
+      this.$refs.editBookModal.hide();
+      this.initForm();
+      this.getBooks();
     },
 
     removeBook() {
